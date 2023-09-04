1d5f550b2303c08270efcaded7aea1fa4dc1b589
Tobias <tobias.vinblad0013@stud.hkr.se>
2023-09-05 00:08:51 +0200

    task 3

---
+++ src/task3/Main.java
@@ -0,0 +1,23 @@
+package task3;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread stopwatch = new Thread(() ->{
+            System.out.println("Stopwatch thread. Stopwatch starts now");
+            float time = 0;
+            try {
+                while (time < 60000){
+                    Thread.sleep(10);
+                    time += 10;
+                    System.out.printf("Stopwatch thread. Elapsed: %.2f seconds.%n", (time / 1000));
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        stopwatch.start();
+        System.out.println("Main thread. Waiting for stopwatch thread...");
+        System.out.println("Main thread. Finished stopwatch thread.");
+    }
+}
