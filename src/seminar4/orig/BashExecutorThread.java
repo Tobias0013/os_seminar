0a27ba0024f8b7e48405ae7bc92cbc39cf87c1af
Tobias <tobias.vinblad0013@stud.hkr.se>
2023-10-03 17:47:41 +0200

    seminar4 structure

---
+++ src/seminar4/orig/BashExecutorThread.java
@@ -0,0 +1,66 @@
+package seminar4.orig;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class BashExecutorThread implements Runnable{
+
+    List<String> input;
+    List<String[]> log;
+
+    public BashExecutorThread(List<String> command, List<String[]> log) {
+        this.input = command;
+        this.log = log;
+    }
+
+    public BashExecutorThread(List<String> input) {
+        this.input = input;
+    }
+
+    public List<String> getInput() {
+        return input;
+    }
+
+    public void setInput(List<String> input) {
+        this.input = input;
+    }
+
+    @Override
+    public void run() {
+        ProcessBuilder processBuilder = new ProcessBuilder(input);
+
+        BufferedReader bufferReader = null;
+
+        try {
+            Process process = processBuilder.start();
+            InputStream inputStream = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(inputStream);
+            bufferReader = new BufferedReader(isr);
+
+            String line;
+            while ((line = bufferReader.readLine()) != null) {
+                System.out.println(line );
+            }
+            bufferReader.close();
+
+        } catch (IOException ioe) {
+            System.err.println("Error");
+            System.err.println(ioe);
+            this.log.add(new String[]{input.toString().replace("[", "").replace("]", ""),
+                    ioe.getMessage()});
+        } finally {
+            if (bufferReader != null) {
+                try {
+                    bufferReader.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                    this.log.add(new String[]{input.toString().replace("[", "").replace("]", ""),
+                            e.getMessage()});
+                }
+            }
+        }
+    }
+}
---
+++ src/seminar4/solution/Main.java
@@ -0,0 +1,64 @@
+package seminar4.solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+//command to compile, javac seminar1.Main.java seminar1.BashExecutorThread.java
+
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        String commandLine;
+        List<String> input;
+        List<String[]> errors = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n\n***** Welcome to the Java Command Shell *****");
+        System.out.println("If you want to exit the shell, type END and press RETURN.\n");
+
+        while (true) {
+            System.out.print("jsh>");
+            commandLine = scanner.nextLine();
+            // if user entered a return, just loop again
+            if (commandLine.equals("")) {
+                continue;
+            }
+
+            input = Arrays.asList(commandLine.split(" "));
+
+            if (commandLine.equalsIgnoreCase("end")) { //User wants to end shell
+                System.out.println("\n***** Command Shell Terminated. See you next time. BYE for now. *****\n");
+                scanner.close();
+                System.exit(0);
+            }
+            else if (commandLine.equalsIgnoreCase("showerrlog")) {
+                System.out.println("Type showerrlog [number] to show the error code");
+                for (int i = 0; i < errors.size(); i++) {
+                    System.out.printf("%s:%s%n",i , errors.get(i)[0]);
+                }
+            }
+            else if (input.get(0).equalsIgnoreCase("showerrlog")) {
+                showerrlog(input, errors);
+            }
+            else {
+                BashExecutorThread bet = new BashExecutorThread(input, errors);
+                Thread thread = new Thread(bet);
+                thread.start();
+            }
+        }
+    }
+
+    public static void showerrlog(List<String> input, List<String[]> errors){
+        if (!input.get(1).matches("\\d+")) {
+            System.out.println("showerrlog syntax error. syntax: showerrlog [number]");
+            return;
+        }
+        else if (Integer.parseInt(input.get(1)) < 0 || Integer.parseInt(input.get(1)) > errors.size()){
+            System.out.println("showerrlog number out of range. syntax: showerrlog [number]");
+            return;
+        }
+        System.out.printf("User input: %s%n", errors.get(Integer.parseInt(input.get(1)))[0]);
+        System.out.printf("Error message: %s%n", errors.get(Integer.parseInt(input.get(1)))[1]);
+
+    }
+}
